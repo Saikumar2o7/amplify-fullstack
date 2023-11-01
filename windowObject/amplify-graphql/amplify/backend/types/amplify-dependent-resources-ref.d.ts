7390b3f25c9a69908d0138d1146778b4485190fd
Saikumar2o7 <saikumar22097@gmail.com>
2023-11-01 23:25:22 +0530

    final commit

---
+++ windowObject/amplify-graphql/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyfullstack": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
